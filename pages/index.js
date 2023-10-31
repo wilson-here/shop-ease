a7f8fc98a24c5286e22c2c7c948214e104f8452d
wilsonandhisgreenbike <quanganhha99@gmail.com>
2023-10-31 15:03:57 +0700

    fix layout done

--- pages/index.js
+++ pages/index.js
@@ -17,15 +17,13 @@ const Home = ({ products, bannerData }) => {
         </p>
       </div>
       <div className="flex flex-wrap gap-y-4 min-[480px]:-mx-2 min-[480px]:gap-y-2 sm:-mx-3 sm:gap-y-3 md:-mx-4 md:gap-y-4">
-        {products
-          ?.filter((product, i) => i <= 7)
-          .map((product) => (
-            <Product
-              key={product._id}
-              product={product}
-              className="w-full min-[480px]:px-2 min-[480px]:w-1/2 sm:px-3 sm:w-1/3 md:px-4 md:w-1/4 "
-            />
-          ))}
+        {products?.slice(-8).map((product) => (
+          <Product
+            key={product._id}
+            product={product}
+            className="w-full min-[480px]:px-2 min-[480px]:w-1/2 sm:px-3 sm:w-1/3 md:px-4 md:w-1/4 "
+          />
+        ))}
       </div>
       <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </div>
